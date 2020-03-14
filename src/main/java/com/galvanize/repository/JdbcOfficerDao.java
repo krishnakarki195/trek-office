3de04c5b46401fe627e8cbcc3cfc533f2c4cf0a8
krishnakarki195 <krishna.karki195@gmail.com>
2020-03-14 13:17:35 -0500

    Complete first assignments

--- src/main/java/com/galvanize/repository/JdbcOfficerDao.java
+++ src/main/java/com/galvanize/repository/JdbcOfficerDao.java
@@ -1,17 +1,83 @@
 package com.galvanize.repository;
 
 
+import com.galvanize.entities.Officer;
+import com.galvanize.entities.Rank;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Repository
 public class JdbcOfficerDao {
 
     JdbcTemplate jdbcTemplate;
 
+    SimpleJdbcInsert insertOfficer;
+
     public JdbcOfficerDao(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+        insertOfficer = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("officers")
+                .usingGeneratedKeyColumns("id");
     }
 
+    public Long count(){
+        Long totalCount;
+        try {
+            String query = "SELECT COUNT(*) FROM officers";
+            totalCount = jdbcTemplate.queryForObject(query, Long.class);
+        }
+        catch (EmptyResultDataAccessException e) {
+            return null;
+        }
+        return totalCount;
+    }
+
+    public List<Officer> findAll(){
+        String query = "SELECT * FROM officers;";
+        return jdbcTemplate.query(
+                query,
+                (rs, rowNum) ->
+                        new Officer(
+                                rs.getLong("id"),
+                                Rank.valueOf(rs.getString("officer_rank")),
+                                rs.getString("first_name"),
+                                rs.getString("last_name")
+                        )
+        );
+    }
+
+    public Officer findById(Long id){
+        String query = "SELECT * FROM officers WHERE id = ?";
+        try{
+            return jdbcTemplate.queryForObject( query, (rs, rowNum) -> new Officer(rs.getLong("id"),
+                    Rank.valueOf(rs.getString("officer_rank")),
+                    rs.getString("first_name"),
+                    rs.getString("last_name")), id);
+        }catch(EmptyResultDataAccessException e){
+            return null;
+        }
+    }
+
+    public Officer save(Officer officer) {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("officer_rank", officer.getRank());
+        parameters.put("first_name", officer.getFirstName());
+        parameters.put("last_name", officer.getLastName());
+        long newId = insertOfficer.executeAndReturnKey(parameters).longValue();
+        officer.setId(newId);
+        return officer;
+    }
+
+    public boolean delete(Long id){
+        String query = "DELETE FROM officers WHERE id = ?";
+        Object[] args = new Object[] {id};
+        return jdbcTemplate.update(query, args) == 1;
+    }
 
 }
---
+++ src/test/java/com/galvanize/JdbcOfficerDoaTest.java
@@ -0,0 +1,50 @@
+package com.galvanize;
+
+import com.galvanize.entities.Officer;
+import com.galvanize.entities.Rank;
+import com.galvanize.repository.JdbcOfficerDao;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class JdbcOfficerDoaTest {
+
+    @Autowired
+    JdbcOfficerDao jdbcOfficerDao;
+
+    @Test
+    public void countOfficersTest(){
+        long totalOfficer = jdbcOfficerDao.count();
+        assertEquals(totalOfficer,5);
+    }
+
+    @Test
+    public void findAllOfficersTest(){
+        assertNotNull(jdbcOfficerDao.findAll());
+        assertEquals(jdbcOfficerDao.findAll().size(),5);
+    }
+
+    @Test
+    public void officerExistsByIdTest(){
+        assertNull(jdbcOfficerDao.findById(100L));
+        assertNotNull(jdbcOfficerDao.findById(5L));
+    }
+
+    @Test
+    public void createNewOfficerTest(){
+        Officer officer = jdbcOfficerDao.save(new Officer(Rank.CAPTAIN,"Michael", "Johnson"));
+        assertEquals(officer.getFirstName(),"Michael");
+        assertEquals(officer.getLastName(),"Johnson");
+        assertEquals(officer.getRank(),Rank.CAPTAIN);
+    }
+
+    @Test
+    public void deleteOfficerTest(){
+        assertEquals(jdbcOfficerDao.delete(5L), true);
+        assertEquals(jdbcOfficerDao.delete(100L), false);
+    }
+
+
+}
